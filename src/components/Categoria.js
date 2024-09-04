f2e4ea4429f72a1bf8c177182114886a13e3549c
crison14 <cris.ro.ag14@gmail.com>
2024-09-04 04:43:55 -0400

    Se agregaron animaciones

--- src/components/Categoria.js
+++ src/components/Categoria.js
@@ -22,20 +22,18 @@ function Categoria() {
   return (
     <div className="p-5">
       <h1 className="text-3xl font-bold mb-4">{categoria.toUpperCase()}</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="cards grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {tools.map(tool => (
-          <div key={tool.id} className="card bg-base-100 shadow-xl">
-            <figure className="px-10 pt-10">
+        <a href={tool.link} target="_blank" rel="noopener noreferrer" className="card boton-elegante">
+
+            <figure className="px-1 pt-1">
               <img src={tool.foto} alt={tool.titulo} className="rounded-xl"/>
             </figure>
             <div className="card-body items-center text-center">
               <h2 className="card-title">{tool.titulo}</h2>
-              <p>{tool.descripcion}</p>
-              <div className="card-actions">
-                <a href={tool.link} target="_blank" rel="noopener noreferrer" className="btn btn-primary">Visitar</a>
-              </div>
-            </div>
+
           </div>
+        </a>
         ))}
       </div>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -19,45 +19,45 @@ const Header = () => {
     <nav className="bg-gray-800 p-4">
       <div className="container mx-auto flex flex-wrap items-center justify-between">
         {/* Logo o Título */}
-        <div className="text-white text-2xl font-bold">Mis herramientas</div>
+        <div className="text-white text-2xl font-bold text-center items-center">Mis herramientas</div>
 
         {/* Navbar */}
         <div className="w-full block lg:flex lg:items-center lg:justify-between">
           <div className="flex flex-wrap justify-center lg:justify-start space-x-2 lg:space-x-4">
-            <Link to="/iconos" className="button mb-2 lg:mb-0" title="Iconos">
+            <Link to="/iconos" className="boton-elegante mb-2 lg:mb-0" title="Iconos">
               <FontAwesomeIcon icon={faIcons} size="lg" />
             </Link>
-            <Link to="/3d" className="button mb-2 lg:mb-0" title="3D">
+            <Link to="/3d" className="boton-elegante mb-2 lg:mb-0" title="3D">
               <FontAwesomeIcon icon={faCube} size="lg" />
             </Link>
-            <Link to="/librerias" className="button mb-2 lg:mb-0" title="Librerías">
+            <Link to="/librerias" className="boton-elegante mb-2 lg:mb-0" title="Librerías">
               <FontAwesomeIcon icon={faCode} size="lg" />
             </Link>
-            <Link to="/illustraciones" className="button mb-2 lg:mb-0" title="Illustraciones">
+            <Link to="/illustraciones" className="boton-elegante mb-2 lg:mb-0" title="Illustraciones">
               <FontAwesomeIcon icon={faImage} size="lg" />
             </Link>
-            <Link to="/fotos" className="button mb-2 lg:mb-0" title="Fotos">
+            <Link to="/fotos" className="boton-elegante mb-2 lg:mb-0" title="Fotos">
               <FontAwesomeIcon icon={faImage} size="lg" />
             </Link>
-            <Link to="/videos" className="button mb-2 lg:mb-0" title="Videos">
+            <Link to="/videos" className="boton-elegante mb-2 lg:mb-0" title="Videos">
               <FontAwesomeIcon icon={faVideo} size="lg" />
             </Link>
-            <Link to="/blogs" className="button mb-2 lg:mb-0" title="Blogs">
+            <Link to="/blogs" className="boton-elegante mb-2 lg:mb-0" title="Blogs">
               <FontAwesomeIcon icon={faBlog} size="lg" />
             </Link>
-            <Link to="/herramientas" className="button mb-2 lg:mb-0" title="Herramientas">
+            <Link to="/herramientas" className="boton-elegante mb-2 lg:mb-0" title="Herramientas">
               <FontAwesomeIcon icon={faTools} size="lg" />
             </Link>
-            <Link to="/colores" className="button mb-2 lg:mb-0" title="Colores">
+            <Link to="/colores" className="boton-elegante mb-2 lg:mb-0" title="Colores">
               <FontAwesomeIcon icon={faPalette} size="lg" />
             </Link>
-            <Link to="/fondos" className="button mb-2 lg:mb-0" title="Fondos">
+            <Link to="/fondos" className="boton-elegante mb-2 lg:mb-0" title="Fondos">
               <FontAwesomeIcon icon={faImage} size="lg" />
             </Link>
-            <Link to="/inspiracion" className="button mb-2 lg:mb-0" title="Inspiración">
+            <Link to="/inspiracion" className="boton-elegante mb-2 lg:mb-0" title="Inspiración">
               <FontAwesomeIcon icon={faWandSparkles} size="lg" />
             </Link>
-            <Link to="/componentes" className="button mb-2 lg:mb-0" title="Componentes">
+            <Link to="/componentes" className="boton-elegante mb-2 lg:mb-0" title="Componentes">
               <FontAwesomeIcon icon={faPuzzlePiece} size="lg" />
             </Link>
           </div>
