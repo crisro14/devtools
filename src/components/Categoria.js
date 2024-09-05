97d6f0de45864a728b1fe6f55c8c043a7a0e8edf
crison14 <cris.ro.ag14@gmail.com>
2024-09-05 03:44:23 -0400

    +

--- src/components/Categoria.js
+++ src/components/Categoria.js
@@ -24,13 +24,13 @@ function Categoria() {
       <h1 className="text-3xl font-bold mb-4">{categoria.toUpperCase()}</h1>
       <div className="cards grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {tools.map(tool => (
-        <a href={tool.link} target="_blank" rel="noopener noreferrer" className="card boton-elegante">
+        <a href={tool.link} target="_blank" rel="noopener noreferrer" className="card boton-elegante ">
 
-            <figure className="px-1 pt-1">
+            <figure className="px-1 pt-1 pad-negativo">
               <img src={tool.foto} alt={tool.titulo} className="rounded-xl"/>
             </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">{tool.titulo}</h2>
+            <div className="card-body items-center text-center pad-negativo">
+              <h2 className="card-title text-bold">{tool.titulo}</h2>
               <p className="card-text">{tool.descripcion}</p>
           </div>
         </a>
